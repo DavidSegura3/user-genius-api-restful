6ff6ca7b2aecaf4d7a953d775e6f056664bbcf8e
DavidSegura3 <seguradavid7@gmail.com>
2024-08-04 18:23:18 -0500

    commit init

---
+++ src/main/java/com/nisum/api/cl/UserGeniusApiRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.nisum.api.cl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserGeniusApiRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserGeniusApiRestfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nisum/api/cl/UserGeniusApiRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nisum.api.cl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserGeniusApiRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
